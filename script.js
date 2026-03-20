2a3b7b1b1b19a61f4589023e7c95eb35c2a9701c
Sunil Kumar Yadav <ksurajyadav93@gmail.com>
2026-03-20 18:06:33 +0530

    Add new song and update recommended songs display

--- script.js
+++ script.js
@@ -29,7 +29,9 @@ const myFixedSongs = [
     { name: 'Phir Bhi Tumko Chaahunga', artist: 'Arijit Singh', img: 'https://image2url.com/r2/default/images/1773863572773-8f31b0fb-71b0-4e9d-b91d-2fa1d7e33b1c.jpg', url: 'https://image2url.com/r2/default/audio/1773863766875-ff5a1da7-fa34-4a22-8bd8-8e97c524237e.mp3' },
     { name: 'Sanson Ki Mala', artist: 'Rahat Fateh Ali Khan', img: 'https://image2url.com/r2/default/images/1773866445142-a5076e4b-a813-4553-9de7-72170dc2c85f.jpg', url: 'https://image2url.com/r2/default/audio/1773866292593-eeb5fc72-d3b0-4e77-9a8d-0c3076fec763.mp3' },
     { name: 'Chaap Tilak', artist: 'Nakash Aziz', img: 'https://image2url.com/r2/default/images/1773866751399-f93751a0-62b4-4dd4-811d-3f02024650f4.jpg', url: 'https://image2url.com/r2/default/audio/1773866515382-37a07c64-a3a1-40e4-a311-a97cba1f50f1.mp3' },
-    { name: 'Sukoon', artist: 'Arijit Singh', img: 'https://image2url.com/r2/default/images/1773914133996-4f05ec84-8944-4d10-8565-635d99213067.jpg', url: 'https://image2url.com/r2/default/audio/1773913962606-27f9484e-9948-4ef9-9969-e93fd31a0208.mp3' }
+    { name: 'Sukoon', artist: 'Arijit Singh', img: 'https://image2url.com/r2/default/images/1773914133996-4f05ec84-8944-4d10-8565-635d99213067.jpg', url: 'https://image2url.com/r2/default/audio/1773913962606-27f9484e-9948-4ef9-9969-e93fd31a0208.mp3' },
+    { name: 'Mitti Ke Bete', artist: 'Mithoon', img: 'https://image2url.com/r2/default/images/1774008590271-1bf98070-1f16-4dc5-a0b6-33122f0962a1.jpg', url: 'https://image2url.com/r2/default/audio/1774008803176-8e5f8f3c-dd61-4e48-ac3f-11b6cb026593.mp3' },
+
 ];
 
 // --- Audio Controls ---
@@ -192,7 +194,7 @@ function cardHtml(s, docId = null) {
 
 function loadContent() {
     document.getElementById('favGrid').innerHTML = myFixedSongs.slice(0, 2).map(s => cardHtml(s)).join('');
-    document.getElementById('recommendedGrid').innerHTML = myFixedSongs.slice(2, 4).map(s => cardHtml(s)).join('');
+    document.getElementById('recommendedGrid').innerHTML = myFixedSongs.slice(2, 8).map(s => cardHtml(s)).join('');
     
     db.collection("songs").orderBy("timestamp", "desc").onSnapshot(snap => {
         const cloudGrid = document.getElementById('cloudGrid');
